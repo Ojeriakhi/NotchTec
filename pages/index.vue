b90d4ced71cd24fc827b9ea01b7c49b881acddb0
Ojeriakhi <joyojeey@gmail.com>
2025-02-18 12:45:18 +0100

    i made changes

--- pages/index.vue
+++ pages/index.vue
@@ -59,7 +59,7 @@
    </section>
    <section class="mt-16 py-10 bg-[#0C111D] px-28 flex-col flex justify-center align-center overflow-x-hidden  ">
         <span class="text-white text-sm text-center align-center flex justify-center">Join 4,000+ companies already growing</span>
-        <div class="flex gap-10 pt-7">
+        <div class="flex gap-10 pt-7 justify-center">
             <img src="/images/company1.png" alt="">
             <img src="/images/company2.png" alt="">
             <img src="/images/company3.png" alt="">
@@ -82,48 +82,51 @@
         </section>
 
 
-        <section class="flex mt-8 gap-24 items-center mb-4 jusitify-center align-center overflow-x-hidden ">
-            <div class="px-4 bg-[#F9FAFB] h-fit pb-4">
-              <div class="mt-4">
-                <img src="/images/featuredicon.png" alt="">
-              </div>
+        <section class="grid grid-cols-3 mt-8 gap-8 w-full overflow-x-hidden">
+    <div class="px-4 bg-[#F9FAFB] h-full w-fit pb-4 flex flex-col">
+        <div class="mt-4">
+            <img src="/images/featuredicon.png" alt="">
+        </div>
 
-              <h4 class="mt-6 text-[#101828]">Software Development</h4>
-              <p class="mt-2 text-[#475467]">We specialize in developing high-quality <br> software solutions that enhance efficiency <br> and productivity.</p>
+        <h4 class="mt-6 text-[#101828]">Software Development</h4>
+        <p class="mt-2 text-[#475467] flex-grow">
+            We specialize in developing high-quality <br> software solutions that enhance efficiency <br> and productivity.
+        </p>
 
-              <p class="mt-4 text-[#475467]">Our team creates web and mobile <br> applications, builds enterprise software, and <br> delivers cloud-based applications tailored to <br> meet specific business needs.</p>
+        <p class="mt-4 text-[#475467] flex-grow">
+            Our team creates web and mobile <br> applications, builds enterprise software, and <br> delivers cloud-based applications tailored to <br> meet specific business needs.
+        </p>
 
+        <span class="text-[#007AFF] text-sm mt-auto">View integration</span>
+    </div>
 
-              <span class="text-[#007AFF] text-sm mt-2">View integration</span>
+    <div class="px-4 bg-[#F9FAFB] h-fit w-fit pb-4 flex flex-col">
+        <div class="mt-4">
+            <img src="/images/featuredicon.png" alt="">
+        </div>
 
-            </div>
-            
+        <h4 class="mt-6 text-[#101828]">IT Supplies</h4>
+        <p class="mt-2 text-[#475467] flex-grow">
+            We provide a comprehensive range of IT <br> hardware and software solutions to support <br> business operations. Additionally, we supply <br> software licenses, subscriptions, and IT <br> security solutions to ensure systems run <br>smoothly and securely.
+        </p>
 
-            <div class="px-4 bg-[#F9FAFB] h-fit pb-4">
-              <div class="mt-4">
-                <img src="/images/featuredicon.png" alt="">
-              </div>
+        <span class="text-[#007AFF] text-sm mt-auto">View integration</span>
+    </div>
 
-              <h4 class="mt-6 text-[#101828]">IT Supplies</h4>
-              <p class="mt-2 text-[#475467]">We provide a comprehensive range of IT <br> hardware and software solutions to support <br> business operations.  Additionally, we supply <br> software licenses, subscriptions, and IT <br> security solutions to ensure systems run <br> smoothly and securely.</p>
+    <div class="px-4 bg-[#F9FAFB] h-fit w-fit pb-4 flex flex-col">
+        <div class="mt-4">
+            <img src="/images/featuredicon.png" alt="">
+        </div>
 
-              <span class="text-[#007AFF] text-sm mt-2">View integration</span>
+        <h4 class="mt-6 text-[#101828]">IT Consultancy</h4>
+        <p class="mt-2 text-[#475467] flex-grow">
+            Our skilled consultants assist businesses in <br> enhancing their IT strategies by providing <br> expert guidance and tailored solutions.
+        </p>
 
-            </div>
-            
+        <span class="text-[#007AFF] text-sm mt-auto">View integration</span>
+    </div>
+</section>
 
-            <div class="px-4 bg-[#F9FAFB] h-fit pb-4">
-              <div class="mt-4">
-                <img src="/images/featuredicon.png" alt="">
-              </div>
-
-              <h4 class="mt-6 text-[#101828]">IT Consultancy</h4>
-              <p class="mt-2 text-[#475467]">Our skilled consultants assist businesses in <br> enhancing their IT strategies by providing <br> expert guidance and tailored solutions</p>
-              <span class="text-[#007AFF] text-sm mt-2">View integration</span>
-
-            </div>
-
-        </section>
 
        
 
@@ -136,9 +139,22 @@
             <p class="text-[#101828] text-3xl font-semibold mt-8">
                 Love the simplicity of the <br> service and the prompt <br> customer support. We <br> can’t imagine working <br> without it.
             </p>
-            <div class="mt-16">
+            <div class="mt-16 flex gap-[200px]">
+                <div class="flex gap-3">
+                    <img src="/images/girlimg.png" alt="" class="w-12 rounded-full ">
+                    <div>
+                        <h3 class="text-[#101828] text-lg text-bold">Caitlyn King</h3>
+                        <span class="text-[#475467] text-lg ">CX Manager, Sisyphus</span>
+                    </div>
 
-                <img src="/images/Attribution.png" alt="">
+                    
+                </div>
+                <div class="flex gap-4">
+                    <button class="rounded-full border text-[#667085] text-bold px-4 py-4 shadow"><-</button>
+                    <button class="rounded-full border text-[#667085] text-bold px-4 py-4 shadow">-></button>
+                </div>
+
+                
             </div>
             </div>
 
